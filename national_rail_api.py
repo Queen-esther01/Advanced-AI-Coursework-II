8e204619da9abf407950a362b2d0529ad0b5083c
Esther Ejidike <ejidikeesther@gmail.com>
2026-05-27 18:59:47 +0100

    hide rail envs

--- national_rail_api.py
+++ national_rail_api.py
@@ -1,24 +1,31 @@
-from zeep import Client, Settings
+import os
+
+from dotenv import load_dotenv
 from requests import Session
 from requests.auth import HTTPBasicAuth
+from zeep import Client, Settings
 from zeep.transports import Transport
 
+load_dotenv()
 
-USERNAME = "wwang"
-PASSWORD = "?i92S6"
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 
 
 WSDL = "https://ojp.nationalrail.co.uk/webservices/jpservices.wsdl"
 
+
 def get_ojp_client():
-    """Return a Zeep Client configured with HTTP Basic Authentication and 
+    """Return a Zeep Client configured with HTTP Basic Authentication and
     settings to ignore sequence order."""
     session = Session()
     session.auth = HTTPBasicAuth(USERNAME, PASSWORD)
-    
+
     # Create settings that ignore sequence order and disable strict mode
-    settings = Settings(strict=False, xml_huge_tree=True, xsd_ignore_sequence_order=True)
-    
+    settings = Settings(
+        strict=False, xml_huge_tree=True, xsd_ignore_sequence_order=True
+    )
+
     transport = Transport(session=session)
     client = Client(WSDL, transport=transport, settings=settings)
     return client
